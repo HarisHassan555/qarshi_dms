912bf01542e42516bbc71e4e34858f42ab7f0e82
iiXamOii <muhammad.zaman@live.co.uk>
2025-06-04 14:38:36 +0500

    updated urls

--- VIM-FE/src/app/utils/urls.ts
+++ VIM-FE/src/app/utils/urls.ts
@@ -1,5 +1,5 @@
 export class urls {
-    static API_URL = 'http://58.27.223.143:28080/VIM/'
+    static API_URL = 'http://192.0.0.203:8080/VIM/'
     static SIGNIN_URL = 'api/auth/login'
     static FORGOT_URL = 'auth/forgot'
     static COUNTRY_URL = 'country'
